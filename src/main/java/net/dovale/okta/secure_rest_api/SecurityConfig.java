357e92f94618a6368254aa59bbae324d7b5d4d4d
Raphael do Vale A. Gomes <raphael@dovale.net>
2018-10-17 01:05:26 -0300

    Spring security and Okta configuration

---
+++ src/main/java/net/dovale/okta/secure_rest_api/SecurityConfig.java
@@ -0,0 +1,8 @@
+package net.dovale.okta.secure_rest_api;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+@EnableWebSecurity
+@EnableResourceServer
+public class SecurityConfig {}

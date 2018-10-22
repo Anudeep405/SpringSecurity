7b3a3d95cd13512fdf4faa3ef17526148c97582b
Raphael do Vale A. Gomes <raphael@dovale.net>
2018-10-21 23:05:27 -0300

    Scope security

--- src/main/java/net/dovale/okta/secure_rest_api/HelloWorldController.java
+++ src/main/java/net/dovale/okta/secure_rest_api/HelloWorldController.java
@@ -1,7 +1,10 @@
 package net.dovale.okta.secure_rest_api;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 
 @RestController
 public class HelloWorldController {
@@ -11,5 +14,9 @@ public class HelloWorldController {
         return "Hello World";
     }
 
-
+    @PreAuthorize("#oauth2.hasScope('profile')")
+    @GetMapping("/protected/")
+    public String helloWorldProtected(Principal principal) {
+        return "Hello VIP " + principal.getName();
+    }
 }
